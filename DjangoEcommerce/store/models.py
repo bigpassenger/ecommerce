aa0286bbd7c87e59b33f84063901ece6a892d7f6
amin <amin.mottaghi80@gmail.com>
2024-09-20 20:07:56 +0430

    making models better

--- DjangoEcommerce/store/models.py
+++ DjangoEcommerce/store/models.py
@@ -13,11 +13,20 @@ class Category(models.Model):
         verbose_name_plural = 'categories'
     def __str__(self):
         return self.name
+class Company(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    image = models.ImageField(upload_to='company',blank=True)
+
+    class Meta:
+        ordering = ('name',)
+    def __str__(self):
+        return self.name
     
 class Product(models.Model):
     name = models.CharField(max_length=100,unique=True)
     slug = models.SlugField(max_length=250,unique=True)
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    company = models.ForeignKey(Company,on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10,decimal_places=2)
     image = models.ImageField(upload_to='product',blank=True)
     stock = models.IntegerField()
