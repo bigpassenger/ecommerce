f22e0781e75b669f7b15d822422cabf71bda5652
amin <amin.mottaghi80@gmail.com>
2024-09-20 19:10:12 +0430

    creating models classes for the project

--- DjangoEcommerce/store/models.py
+++ DjangoEcommerce/store/models.py
@@ -6,4 +6,20 @@ class Category(models.Model):
     slug = models.SlugField(max_length=250,unique=True)
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='category',blank=True)
+
+    def __str__(self):
+        return self.name
     
+class product(models.Model):
+    name = models.CharField(max_length=100,unique=True)
+    slug = models.SlugField(max_length=250,unique=True)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    image = models.ImageField(upload_to='product',blank=True)
+    stock = models.IntegerField()
+    available = models.BooleanField(default='True')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
