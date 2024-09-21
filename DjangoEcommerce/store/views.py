04b01a09751c6dc9857a941d6c2f0fb1f53ada80
amin <amin.mottaghi80@gmail.com>
2024-09-21 19:08:25 +0330

    making mobile templates

--- DjangoEcommerce/store/views.py
+++ DjangoEcommerce/store/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Category,Product
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    return render(request,'home.html',{"categories":categories,
+                                       'products':products})
 def product(request):
     return render(request,'product.html')
 def laptops(request):
